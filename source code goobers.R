ed8f6ae7be874f2d0ff38e207738ec7e9d4199c6
Ong Zi Yang <celeskyanpu@gmail.com>
2024-03-26 13:32:26 +0800

    base file
    base file push

--- source code goobers.R
+++ source code goobers.R
@@ -1,4 +1,49 @@
-cat("This is a test print")
+# PFDA Group Assignment
+# To determine the correlation between a person's credit score and other factors, such as monthly in-hand salaries, number of credit cards,
+# number of bank accounts, as well as the percentile of interest rate.
 
-testprnt <- "This is a test"
-testprnt
+# Ong Zi Yang TP065229
+# Objective : To investigate the behaviour between credit score and monthly inhand salary
+
+#Analysis 1 : 
+
+#Analysis 2 :
+
+#Analysis 3 :
+
+#Analysis 4 :
+
+# Joshua Tioh Chee Yong TP065839
+# Objective : 
+
+#Analysis 1 :
+
+#Analysis 2 :
+
+#Analysis 3 :
+
+#Analysis 4 :
+
+# Trilester Movees Tirilos TP066460
+# Objective : To investigate the behaviour between credit score and number of credit cards
+
+#Analysis 1 :
+
+#Analysis 2 :
+
+#Analysis 3 :
+
+#Analysis 4 :
+
+# Wong Wei Hann TP065820
+# Objective : 
+
+#Analysis 1 :
+
+#Analysis 2 :
+
+#Analysis 3 :
+
+#Analysis 4 :
+
+
